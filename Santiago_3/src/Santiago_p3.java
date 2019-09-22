e53c2a3e26633e6cf3a367d05133fbbbe66d4c65
FLoridianKing <47996729+FLoridianKing@users.noreply.github.com>
2019-09-21 22:10:56 -0400

    Add files via upload
    Programming Assignment 2 Problem 3

---
+++ Santiago_3/src/Santiago_p3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Santiago_p3 {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		
+		int userNum;
+		String topics[] = {"Health","Runescape", "Family", "School", "Friends" };
+		
+		System.out.println("How many people will participate in this survey? ");
+		
+		userNum = scnr.nextInt(); 
+		
+		
+		System.out.println("Rate each topic from least important to most important: ");
+		System.out.println("");
+		System.out.println("Health, Runescape, Family, School, Friends");
+		
+		String topic = scnr.next();
+		
+		
+		
+
+	}
+
+}
