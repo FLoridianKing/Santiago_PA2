b8e0501f5dd1134453512fd0f7d36285f394c49e
FLoridianKing <47996729+FLoridianKing@users.noreply.github.com>
2019-09-21 22:10:09 -0400

    Add files via upload
    Programming Assignment 2 problem 1 Encrypt/ Decrypt

---
+++ Santiago_p1Encrypt/src/Santiago_p1Decrypt.java
@@ -0,0 +1,9 @@
+
+public class Santiago_p1Decrypt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Santiago_p1Encrypt/src/Santiago_p1Encrypt.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Santiago_p1Encrypt {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		int number;
+		
+		
+		
+		
+		
+		System.out.print("Please enter four numbers to encrypt: ");
+		number = scnr.nextInt();
+		
+		
+		
+		
+		
+		
+	}
+
+}
